213b913c4a7225bcb17d78338975bf1f3d2c8b50
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-07-17 20:54:27 -0300

    Ajustando componentes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Button from './components/Button'
 import Display from './components/Display'
 
+export default class App extends Component {
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      
-      {/** 
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      */}
+  //Manipula o valor do display
+  state = {
+    displayValue: '100'
+  }
 
-      <Display value={0}/>
-      
-      <View style={styles.buttons}>
-        <Button label='AC' />
-        <Button label='/' />
-        <Button label='7' />
-        <Button label='8' />
-        <Button label='9' />
-        <Button label='*' />
-        <Button label='4' />
-        <Button label='5' />
-        <Button label='6' />
-        <Button label='-' />
-        <Button label='1' />
-        <Button label='2' />
-        <Button label='3' />
-        <Button label='+' />
-        <Button label='0' />
-        <Button label='.' />
-        <Button label='=' />
+  //Função para adicionar valores e digitos ao display, tbm limpa
+  addDigito = n => {
+    this.setState({ displayValue: n })
+  }
+  clearMemory = () => {
+    this.setState({ displayValue: '0' })
+  }
+  setOperation = operation =>{
+    this.setState({ displayValue: operation })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Display value={this.state.displayValue}/>
+        <View style={styles.buttons}>
+          <Button nomeBtn='AC' triple onClick={this.clearMemory}/>
+          <Button nomeBtn='/'  operation/>
+          <Button nomeBtn='7' />
+          <Button nomeBtn='8' />
+          <Button nomeBtn='9' />
+          <Button nomeBtn='*' operation/>
+          <Button nomeBtn='4' />
+          <Button nomeBtn='5' />
+          <Button nomeBtn='6' />
+          <Button nomeBtn='-' operation/>
+          <Button nomeBtn='1' />
+          <Button nomeBtn='2' />
+          <Button nomeBtn='3' />
+          <Button nomeBtn='+' operation/>
+          <Button nomeBtn='0' />
+          <Button nomeBtn='.' double />
+          <Button nomeBtn='=' operation />
+        </View>  
       </View>
-
-    </View>
-  );
+    );
+  }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -50,3 +62,13 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap'
   }
 });
+
+/**
+ *  Componente Botão passará um nome, possivelmente uma
+ *  personalização diferente de css que definirá seu tamanho,
+ *  e uma função para manipular o o estado do valor que será
+ *  exibido no componente Display.  
+ * 
+ *  Componente Display apenas receberá o valor atualizado do
+ *  estado que é manipulado pelas funções dos botoes
+ */
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,25 @@
 import React from 'react'
-import {
-    View,
-    Text,
-    StyleSheet,
-    Dimensions,
-    TouchableHighlight
-} from 'react-native'
+import { Text, StyleSheet, Dimensions, TouchableHighlight } from 'react-native'
 
 export default props =>{
+
+    /*
+        Cria um array com os estilo button e adiciona outros conforme
+        a chamada dos componentes
+    */
+    const stylesButton = [estilo.button]
+    if(props.double) stylesButton.push(estilo.buttonDouble)
+    if(props.triple) stylesButton.push(estilo.buttonTriple)
+    if(props.operation) stylesButton.push(estilo.operationButton)
+
     return(
         <TouchableHighlight onPress={props.onClick}>
-            <Text style={estilo.button}>{props.label}</Text>
+            <Text style={stylesButton}>{props.nomeBtn}</Text>
         </TouchableHighlight>
     )
 }
+
+
 
 const estilo = StyleSheet.create({
     button: {
@@ -25,5 +31,15 @@ const estilo = StyleSheet.create({
         textAlign: "center",
         borderWidth: 1,
         borderColor: '#888'
+    },
+    operationButton: {
+        color: '#fff',
+        backgroundColor: "#fa8231",
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width /4) *2,
+    },
+    buttonTriple: {
+        width: (Dimensions.get('window').width / 4) * 3,
     }
 })
